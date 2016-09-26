1549e1a2ba96161a6cf226332a6ba8db41fa76ba
Iris Inami <irisinami@iriss-mac.local>
2016-09-26 04:19:56 -0700

    09/26/16 progress; designing UI and menus

---
+++ SMF/FeedTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FeedTableViewController.swift
+//  SMF
+//
+//  Created by Iris Inami on 2016-09-25.
+//  Copyright © 2016 Iris Inami. All rights reserved.
+//
+
+
+
+
+import UIKit
+
+class FeedTableViewController: UITableViewController {
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
