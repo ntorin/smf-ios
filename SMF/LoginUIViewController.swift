86e14f2220a299da4df49a75cd03de3a8a980221
Iris Inami <irisinami@Iriss-Mac.local>
2016-10-01 16:21:08 -0700

    09/30/16 progress; creating sign-in page and adding account creation functionality

---
+++ SMF/LoginUIViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginUIViewController.swift
+//  SMF
+//
+//  Created by Iris Inami on 2016-10-01.
+//  Copyright © 2016 Iris Inami. All rights reserved.
+//
+
+import UIKit
+
+class LoginUIViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
